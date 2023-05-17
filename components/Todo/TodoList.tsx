04cfac619579f4824cf72a6257001d704313aa11
Insu Hong <hinsu93@gmail.com>
2023-05-17 15:08:47 +0900

    create TodoList

--- components/Todo/TodoList.tsx
+++ components/Todo/TodoList.tsx
@@ -1,9 +1,35 @@
-const TodoList = () => {
+import { useRecoilValue } from "recoil";
+import { TodoType, todoTypeAtom } from "./atom";
+import TodoItem from "./TodoItem";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchTodo = async (todoType = TodoType.TODO) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACK_API}/api/todo?todoType=${todoType}`
+  );
+  const json = await response.json();
+  return json;
+};
+
+const useTodo = (todoType: TodoType) => {
+  return useQuery({
+    queryKey: ["todo", todoType],
+    queryFn: () => fetchTodo(todoType),
+    staleTime: 3 * 60 * 1000,
+  });
+};
+
+const ToDoList = () => {
+  const todoType = useRecoilValue(todoTypeAtom);
+  const { data } = useTodo(todoType);
+
   return (
     <>
-      <div></div>
+      {data?.map((item) => (
+        <TodoItem key={item._id} {...item} />
+      ))}
     </>
   );
 };
 
-export default TodoList;
+export default ToDoList;
