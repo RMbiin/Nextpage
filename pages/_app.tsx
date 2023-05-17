8527743f193464ddf8296b4a90242b430f188015
Insu Hong <hinsu93@gmail.com>
2023-05-17 14:09:23 +0900

    _app 파일 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,26 @@
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import Layout from "@/components/Layout";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.results}>
+        <ReactQueryDevtools initialIsOpen={true} />
+        <RecoilRoot>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RecoilRoot>
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
