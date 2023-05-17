59cd259b358de8d356349cef064d6b8f01d7fa58
Insu Hong <hinsu93@gmail.com>
2023-05-17 14:58:39 +0900

    create TodoTab

--- components/Todo/TodoTab.tsx
+++ components/Todo/TodoTab.tsx
@@ -1,7 +1,46 @@
+import { useRecoilState } from "recoil";
+import { styled } from "styled-components";
+import { TodoType, todoTypeAtom } from "./atom";
+
+const Wrapper = styled.div`
+  .selected {
+    color: red;
+  }
+`;
+
 const TodoTab = () => {
+  const [todoType, setTodoType] = useRecoilState(todoTypeAtom);
+
+  const handleTodoType = (event: React.MouseEvent) => {
+    const selectedType = event.currentTarget.id as TodoType;
+    setTodoType(() => selectedType);
+  };
+
   return (
     <>
-      <div></div>
+      <Wrapper>
+        <span
+          id={TodoType.TODO}
+          onClick={handleTodoType}
+          className={todoType === TodoType.TODO ? "selected" : ""}
+        >
+          할 일
+        </span>
+        <span
+          id={TodoType.DOING}
+          onClick={handleTodoType}
+          className={todoType === TodoType.DOING ? "selected" : ""}
+        >
+          작업중
+        </span>
+        <span
+          id={TodoType.DONE}
+          onClick={handleTodoType}
+          className={todoType === TodoType.DONE ? "selected" : ""}
+        >
+          완 료
+        </span>
+      </Wrapper>
     </>
   );
 };
